1c380feaf687f67e3f9f444915d3e9099798ee2c
TekkenPlayer <robert.sepulveda1995@gmail.com>
2021-03-10 21:51:33 -0300

    se arregla peticion query en el archivo queries.js

--- config/queries.js
+++ config/queries.js
@@ -1,7 +1,7 @@
 module.exports = {
     crud: {
         read: "SELECT * FROM Operations ",
-        readHome: "SELECT * FROM Operations ORDER BY ID ASC limit 10",
+        readHome: "SELECT * FROM Operations ORDER BY ID DESC limit 10",
         post: "INSERT INTO Operations (Concept, Mount, DATE, OperationType) VALUES (?,?,?,?)",
         del: "DELETE FROM Operations WHERE ID = ?",
         update: "UPDATE Operations SET ? WHERE ID =?",
